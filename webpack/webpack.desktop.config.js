6987fcf8774f355c7c0b392f7fdc570155206cb8
Stephen Hallett <stephen.hallett@catchgroup.com.au>
2018-09-14 15:10:24 +1000

    fix webpack hopefully

--- webpack/webpack.desktop.config.js
+++
@@ -1,7 +0,0 @@
-var webpack = require('webpack');
-var config = require('./webpack.min.config');
-
-config.target = "electron";
-config.plugins.push(new webpack.DefinePlugin({WEBPACK_DEF_TARGET_WEB: false}));
-
-module.exports = config;
